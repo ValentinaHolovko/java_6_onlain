4eac38f1cfd0240fee4f9622bbc68f5b46142028
Valentina Holovko <119681210+ValentinaHolovko@users.noreply.github.com>
2023-05-03 21:48:50 +0300

    hw 10

---
+++ src/main/java/ua/com/alevel/exercise1/Aspirant.java
@@ -0,0 +1,43 @@
+package ua.com.alevel.exercise1;
+
+import java.util.Objects;
+
+public class Aspirant extends Student {
+    private String scientificWork;
+
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String scientificWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scientificWork = scientificWork;
+    }
+
+    public String getScientificWork() {
+        return scientificWork;
+    }
+
+    public void setScientificWork(String scientificWork) {
+        this.scientificWork = scientificWork;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), scientificWork);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Aspirant aspirant = (Aspirant) o;
+        return Objects.equals(scientificWork, aspirant.scientificWork);
+    }
+
+    @Override
+    public int getScholarship() {
+        if (getAverageMark() == 5) {
+            return 200;
+        } else {
+            return 180;
+        }
+    }
+}
