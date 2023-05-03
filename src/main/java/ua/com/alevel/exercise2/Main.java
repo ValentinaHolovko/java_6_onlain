e041c58e55999a023710c758ce4381041458af13
Valentina Holovko <119681210+ValentinaHolovko@users.noreply.github.com>
2023-05-03 21:54:41 +0300

    hw 11

---
+++ src/main/java/ua/com/alevel/exercise2/Main.java
@@ -0,0 +1,20 @@
+package ua.com.alevel.exercise2;
+
+public class Main {
+    public static void main(String[] args) {
+        Pow pow = (number, p) -> {
+            int result = 1;
+            for (int i = 0; i < p; i++) {
+                result *= number;
+            }
+            return result;
+        };
+
+        int number = 7;
+        int p = 6;
+        int result = pow.pow(number, p);
+        System.out.println(number + " в степени " + p + " = " + result);
+    }
+}
+
+
