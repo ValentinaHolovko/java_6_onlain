54ef2289a1b731fce2e73807b2b7379a9168fcb2
Valentina Holovko <119681210+ValentinaHolovko@users.noreply.github.com>
2023-06-06 21:00:11 +0300

    hw 14

---
+++ src/main/java/ua/holovko/jadv/hw14/MySet.java
@@ -0,0 +1,64 @@
+package ua.holovko.jadv.hw14;
+
+import java.util.Arrays;
+
+public class MySet<T> {
+    private Object[] elements;
+    private int size;
+
+    public MySet(int initialSize) {
+        elements = new Object[initialSize];
+        size = 0;
+    }
+
+    public boolean add(T element) {
+        if (contains(element)) {
+            return false;
+        }
+
+        if (size == elements.length) {
+            resizeArray();
+        }
+
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        return (T) elements[index];
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size - 1; i++) {
+            sb.append(elements[i]).append(", ");
+        }
+        sb.append(elements[size - 1]).append("]");
+        return sb.toString();
+    }
+
+    private boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void resizeArray() {
+        int newCapacity = elements.length * 2;
+        elements = Arrays.copyOf(elements, newCapacity);
+    }
+}
+
